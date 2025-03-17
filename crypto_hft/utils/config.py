201e16403fb3be09783556a21954908a192bb2fa
Andrea Franceschini <andrea.franceschini03@gmail.com>
2025-03-17 11:58:35 -0400

    feat(utils): :sparkles: add config class pulling secrets from .env file

---
+++ crypto_hft/utils/config.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+
+# define the secrets that need to be pulled from the .env file
+SECRETS = [
+    'db_name',
+    'db_user', 
+    'db_password', 
+    'db_host',
+    'db_port'
+]
+
+class Config():
+    """Define config for the whole project, and pull secrets when initializing the class"""
+    # tickers to stream
+    exchanges = ['coinbase', 'hyperliquid', 'poloniex']
+    base_tickers = [
+        "BTC_USDT",
+        "ETH_USDT"
+    ]
+    data_types = [
+        'trade', 'book_snapshot_10_0ms'
+    ]
+
+    def __init__(self): 
+        load_dotenv()
+
+        for secret in SECRETS:
+            secret_name = secret.upper()
+            if os.getenv(secret) is None:
+                raise ValueError(
+                    f"Missing secret: {secret_name}."
+                    "Please create a .env file in the root directory of the project"
+                    f" and add the following env variables to it: {SECRETS}"
+                )
+            setattr(self, secret, os.getenv(secret_name))
